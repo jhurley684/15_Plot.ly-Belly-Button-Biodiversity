01221e1402bcda742868586fdac2a08422bf7f0c
Jim Hurley <jhurley684@gmail.com>
2021-12-13 13:25:37 -0600

    Added drop-down control

--- static/js/app.js
+++ static/js/app.js
@@ -1,10 +1,23 @@
-console.log("This is plot.js")
+// This code taken from Dom's office hours session 12/11
+
+console.log("This is plots.js")
 
 
 
-// function InitDashboard()
-// {
-//         console.log("Initializing Dashboard")
-// }
+function InitDashboard()
+{
+        console.log("Initializing Dashboard")
 
-// InitDashboard();
+        d3.select("#selDataset");
+
+        // read data from dataset
+        d3.jason("samples.json").then(data=> {
+           
+            coulsole.log(data);
+
+            let sampleNames = data
+
+        });
+}
+
+InitDashboard();
