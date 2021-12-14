c370ddefd118d14e17b3f37f25f5df8639fc2755
Jim Hurley <jhurley684@gmail.com>
2021-12-14 12:33:32 -0600

    Working bar chart

--- plots.js
+++ plots.js
@@ -4,6 +4,37 @@ console.log("This is plots.js");
 
 function DrawBarchart(sampleId) {
         console.log(`DrawBarchart(${sampleId})`);
+
+        d3.json("samples.json").then(data => {
+                
+                let samples = data.samples;
+                let resultArray = samples.filter(s => s.id === sampleId);
+                let result = resultArray[0];
+
+                console.log(result);
+
+                let otu_ids = result.otu_ids;
+                let otu_labels = result.otu_labels;
+                let sample_values = result.sample_values;
+                let yticks = otu_ids.slice(0, 10).map(otuId => `OTU ${otuId}`).reverse();
+
+
+                let barData = {
+                        x: sample_values.slice(0, 10).reverse(),
+                        y: yticks, 
+                        type: "bar",
+                        text: otu_labels.slice(0, 10).reverse(),
+                        orientation: "h"
+                };
+
+                let barArray = [barData];
+
+                Plotly.newPlot("bar", barArray);
+
+
+        });
+
+
 }
 
 
@@ -55,6 +86,5 @@ function InitDashboard()
         });
 
 }
-
 
 InitDashboard();
