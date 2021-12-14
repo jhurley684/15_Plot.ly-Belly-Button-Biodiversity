7eff694f1e4bba9d1961da0fbf801ede723ab0d3
Jim Hurley <jhurley684@gmail.com>
2021-12-14 11:27:34 -0600

    Stubs and event handler

--- plots.js
+++ plots.js
@@ -2,7 +2,31 @@
 
 console.log("This is plots.js");
 
+function DrawBarchart(sampleId) {
+        console.log(`DrawBarchart(${sampleId})`);
+}
 
+
+function DrawBubblechart(sampleId) {
+        console.log(`DrawBubblechart(${sampleId})`);
+}
+
+
+function ShowMetadata(sampleId) {
+        console.log(`ShowMetadata(${sampleId})`);
+}
+
+
+
+function optionChanged(id)
+{
+        console.log(`optionChanged(${id})`);
+
+        DrawBarchart(id);
+        DrawBubblechart(id);
+        ShowMetadata(id);
+
+}
 
 
 function InitDashboard()
@@ -20,9 +44,17 @@ function InitDashboard()
                 selector.append("option")
                         .text(sampleId)
                         .property("value", sampleId);
-            })
+            });
+
+            let sampleId = sampleNames[0];
+
+            DrawBarchart(sampleId);
+            DrawBubblechart(sampleId);
+            ShowMetadata(sampleId);
+
         });
 
 }
+
 
 InitDashboard();
